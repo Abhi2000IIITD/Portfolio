81d8a60b4afcc27bfe1c3d92d2cc44088b981b8a
Abhishek Singh <abhishek24008@iiitd.ac.in>
2025-06-19 18:34:18 +0530

    added vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+// });
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // 👈 This is essential for correct deployment on Netlify
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
